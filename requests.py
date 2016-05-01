ad31520f2c5e88cf0fa42d99090a527df6350e7a
Sarah Aladetan <aladetan@mit.edu>
2016-05-01 11:16:13 -0400

    Made skeleton for POST requests.py

--- requests.py
+++ requests.py
@@ -26,18 +26,28 @@ if method_type == "GET":
                 songs_from_db = database.get_song_chords(username)
                 print("<h1>")
                 for song in songs_from_db.keys():
-                    print(song)
+                    print(song + ", \n")
                 print("</h1>")
                 print("<h2>")
                 for song in songs_from_db.keys():
-                    print(songs_from_db[song])
+                    print(songs_from_db[song] + ", \n")
                 print("</h2>")
-                print("<p>" + str(len(songs_from_db)) + "</p>")
     else:
         print("You need to specify a user name an password as GET parameters")
 
 if method_type == "POST":
-    pass
+    if 'username' in form.keys() and 'password' in form.keys():
+        username = form['username'].value
+        password = form['password'].value
+        if users[username] != password:
+            print('Sorry :( %s. Your password is not correct. Try again!' %(username))
+        else:
+            action = form['action'].value
+            if action == 'song-tutor':
+                pass
+    else:
+        print("You need to specify a user name an password as POST parameters")
+
 
 database.connection.close() #DON'T TOUCH THIS   
 print("</html>")
