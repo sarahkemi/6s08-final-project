736b3893224a5bc6d8dbf404b39c54815b032dce
Sarah Aladetan <aladetan@mit.edu>
2016-05-05 14:59:00 -0400

    Added power saving mode

--- song-tutor/song-tutor.ino
+++ song-tutor/song-tutor.ino
@@ -8,6 +8,16 @@
 #include <SparkFunLSM9DS1.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
+#include <Snooze.h>
+
+//Our power saving options and constants, to the rescue!!
+
+#define DORMANTINTERVAL 10000
+uint32_t last_button_press = 0;
+
+// Time for teensy sleep or deepsleep (ms). Change to whatever you need!
+#define TEENSY_SLEEP_TIME 48  
+SnoozeBlock config;
 
 #define BUTTON_WHITE 23
 #define BUTTON_GREEN 22
@@ -30,6 +40,8 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 #define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
 
 #define wifiSerial Serial1          // for ESP chip
+#define WIFI_CH_PD_PIN 17
+
 
 // Wifi options, constants, and variables
 #define VERBOSE_WIFI true          // Verbose ESP8266 output
@@ -79,6 +91,229 @@ int ind = 0;
 ESP8266 wifi = ESP8266(true);  //Change to "true" or nothing for verbose serial output
 Adafruit_SSD1306 display(4);
 LSM9DS1 imu;
+
+class PowerMonitor {
+  const float BATTERY_CAPACITY = 1000;     // battery capacity in mA-h
+  const float BATTERY_VOLTAGE = 5.0;       // V
+  
+  // ALL CURRENTS IN MILLIAMPS! Fill in with your values
+  const float OLED_ON_CURRENT = 24.4*0.85; // Make assumption about % of pixels lit
+  const float OLED_SLEEP_CURRENT = 0.01;
+
+  const float ACCEL_ON_CURRENT = 1.57;
+  const float ACCEL_SLEEP_CURRENT = 1.29;
+  const float GYRO_ON_CURRENT = 5.70;
+  const float GYRO_SLEEP_CURRENT = 1.57;
+  const float MAG_ON_CURRENT = 1.29;
+  const float MAG_SLEEP_CURRENT = 0.77;
+  
+  const float GPS_ON_CURRENT = 24.5;
+  const float GPS_SLEEP_CURRENT = 1.62;
+  
+  const float WIFI_NOTCONNECTED_CURRENT = 68.0;
+  const float WIFI_DEEPSLEEP_CURRENT = 0.001;
+  const float WIFI_MODEM_SLEEP_CURRENT = 15.0;
+
+  const float TEENSY_ON_CURRENT = 3.83;       
+  const float TEENSY_SLEEP_CURRENT = 0.11;
+  const float TEENSY_DEEPSLEEP_CURRENT = 0.02;
+
+  // All VOLTAGES IN VOLTS!  Fill in with your values 
+  const float TEENSY_SUPPLY_VOLTAGE = 3.3;
+  const float GPS_SUPPLY_VOLTAGE = 5.0;
+  const float WIFI_SUPPLY_VOLTAGE = 3.3;
+  const float OLED_SUPPLY_VOLTAGE = 3.3;
+  const float IMU_SUPPLY_VOLTAGE = 3.3;
+
+  // These hold state of the various components
+  uint8_t state_teensy, state_gps, state_wifi, state_oled, state_imu;
+  bool iswifi, isgps, isoled, isimu;    // do these exist (1) or not (0)
+  long int timestamp;                   // current time (in millis)
+  long int time_offset;                 // holds interval for when 
+                                        // Teensy is sleeping, since millis() 
+                                        // doesn't run during Teensy sleep
+
+  float energyConsumed;     // running total of energy consumed, in mJ
+  float batteryCapacity;    // calculated battery capacity, in mJ
+  float percentEnergyLeft;  // percent of battery energy left
+
+  public:
+
+  /* Constructor. Takes in one 8-bit input, c, that determines which
+  components are being used in our system. We assume Teensy is always present!
+  Other components each take 1 bit, as shown below:   
+        c: 0 0 0 0 IMU OLED GPS WIFI
+        So 00001101 = 0x0D would mean GPS is not present */   
+  PowerMonitor(uint8_t c)
+  {     
+
+    timestamp = millis();
+    energyConsumed = 0;
+    batteryCapacity = BATTERY_CAPACITY * BATTERY_VOLTAGE * 3600; // mJoules
+    percentEnergyLeft = 100;  // Start out at 100%!
+
+
+    iswifi = isgps = isoled = isimu = false;    // Assume no components
+    if (c | 0x01 << 0) iswifi = true;     // Add components depending on bits in c
+    if (c | 0x01 << 2) isoled = true;
+    if (c | 0x01 << 3) isimu = true;    
+
+    // initially, all components are on
+    state_teensy = 2;   //0=deepsleep, 1 = sleep, 2 = normal
+    state_wifi = 1;     //0=sleep, 1 = on aka modem-sleep
+    state_oled = 1;     //0=sleep, 1 = on
+    state_imu = 0x05; //[accel,gyro,mag], 0=sleep, 1 =on  - apart of our strategy!!
+  }
+
+  /* Determine the energy consumed since the last update. Updates 
+  class variables energyConsumed and percentEnergyLeft */
+  void updateEnergy() {
+    long int dT = millis() + time_offset - timestamp; //Time since last update
+    float pw = 0;   // power usage during this interval
+
+    // Depending on state of each component and whether it is present, 
+    // choose appropriate power setting
+    if (state_teensy==2) {
+      pw += TEENSY_ON_CURRENT * TEENSY_SUPPLY_VOLTAGE;
+    } else if (state_teensy==1) {
+      pw += TEENSY_SLEEP_CURRENT * TEENSY_SUPPLY_VOLTAGE;
+    } else if (state_teensy==0) {
+      pw += TEENSY_DEEPSLEEP_CURRENT * TEENSY_SUPPLY_VOLTAGE;
+    }
+    if (isoled) pw += (state_oled) ? OLED_ON_CURRENT*OLED_SUPPLY_VOLTAGE : OLED_SLEEP_CURRENT*OLED_SUPPLY_VOLTAGE;    
+    if (isimu) {
+      pw += (state_imu & 0x01) ? ACCEL_ON_CURRENT*IMU_SUPPLY_VOLTAGE : ACCEL_SLEEP_CURRENT*IMU_SUPPLY_VOLTAGE;    
+      pw += (state_imu & (0x01 << 1)) ? GYRO_ON_CURRENT*IMU_SUPPLY_VOLTAGE : GYRO_SLEEP_CURRENT*IMU_SUPPLY_VOLTAGE;    
+      pw += (state_imu & (0x01 << 2)) ? MAG_ON_CURRENT*IMU_SUPPLY_VOLTAGE : MAG_SLEEP_CURRENT*IMU_SUPPLY_VOLTAGE;    
+    }
+
+    // WiFi power depends on whether we are connected to AP, and based on 
+    // how many transmit and receive operations we have undertaken
+    if (iswifi) {
+      if (state_wifi) {
+        if (wifi.isConnected()) {
+          pw += WIFI_MODEM_SLEEP_CURRENT*WIFI_SUPPLY_VOLTAGE;
+          } else {
+          pw += WIFI_NOTCONNECTED_CURRENT*WIFI_SUPPLY_VOLTAGE;
+          }
+      } else {
+        pw += WIFI_DEEPSLEEP_CURRENT*WIFI_SUPPLY_VOLTAGE;
+      }
+    }
+    float deltaEnergy = dT / 1000.0 * pw;
+    energyConsumed += deltaEnergy;  // mJ
+    percentEnergyLeft -= deltaEnergy / batteryCapacity * 100;
+    timestamp = millis() + time_offset;
+  }
+  
+  /* Set power mode of any particular component 
+  Inputs:
+   Arduino String p: can be "teensy", "wifi", "oled", "imu", "gps"
+   8-bit integer m: value depends on component */
+  void setPowerMode(String p, uint8_t m){
+    // Before changing power mode, update the energy consumed for last interval
+    updateEnergy();   
+
+    // Teensy modes are 2: Normal, 1: Sleep, 0: DeepSleep
+    if (p=="teensy") {
+      if (m != state_teensy) {    // Only set mode if different from current state
+        if (m==1) {
+          state_teensy = m;
+          Snooze.sleep(config);
+          // Need to add offset b/c millis doesn't work during sleep
+          time_offset += TEENSY_SLEEP_TIME; 
+          updateEnergy();   // update energy again to capture energy during sleep
+          state_teensy = 2;         
+          } 
+        else if (m==0) {
+          state_teensy = m;
+          Snooze.deepSleep(config);
+          time_offset += TEENSY_SLEEP_TIME;
+          updateEnergy();
+          state_teensy = 2;
+        }
+      }
+    }
+    // OLED modes are 1: Normal, 0: Sleep
+    else if (isoled && (p=="oled")) {
+      if (m != state_oled) {
+        if (m==1) {
+          display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+        } else if (m==0) {
+          display.ssd1306_command(SSD1306_DISPLAYOFF);
+        }
+        state_oled = m;
+      }
+    }
+    // Wifi modes are 1: On, 0: DeepSleep
+    else if (iswifi && (p=="wifi")) {
+      if (m != state_wifi) {
+        if (m==1) {
+          digitalWrite(WIFI_CH_PD_PIN, HIGH);
+        } else if (m==0) {
+          digitalWrite(WIFI_CH_PD_PIN, LOW);
+        }
+        state_wifi = m;
+      }
+      
+    }
+    /*  IMU modes are more complicated because IMU has 3 peripherals. Thus we
+      use the three least-significant bits in m to hold the desired state of
+      each sensor: [0][0][0][0][0][accel][gyro][mag].  So 0x05 means accel and mag on, 
+      gyro sleep */
+    else if (isimu && (p=="imu")) {
+      // Cannot have gyro ON and accel OFF.  Right now this is not
+      // explicitly forbidden, but will not result in intended behavior,
+      // and will create inaccuracy in estimated current.
+      if (m != state_imu) {
+        uint8_t tempRegValue=0;   // Hold register value
+        uint8_t dbits = ~ (m ^ state_imu);    // Find which bits are different
+        if  (dbits & (0x01 << 2) ) {    // If accel bit is diff
+          tempRegValue = imu.xgReadByte(CTRL_REG6_XL);  // read accel register
+          if (m & (0x01 << 2)) {    // if we want to turn ON,
+            tempRegValue |= 0xE0;   // set bits 7:5 to 1, keep other bits
+            imu.xgWriteByte(CTRL_REG6_XL, tempRegValue);
+          } else {                  // turn off
+            tempRegValue &= 0x1F;   // set bits 7:5 to 0, keep other bits
+            imu.xgWriteByte(CTRL_REG6_XL, tempRegValue);
+          }
+        }
+        if  (dbits & (0x01 << 1) ) {  // If gyro bit is different
+          tempRegValue = imu.xgReadByte(CTRL_REG1_G);  
+          if (m & (0x01 << 1)) {
+            tempRegValue |= 0xE0;   //set bits 7:5 to 1, keep other bits
+            imu.xgWriteByte(CTRL_REG1_G, tempRegValue);
+          } else {
+            tempRegValue &= 0x1F;   //set bits 7:5 to 0, keep other bits
+            imu.xgWriteByte(CTRL_REG1_G, tempRegValue);
+          }
+        }
+        if  (dbits & 0x01) {        // If mag bit is different
+          tempRegValue = imu.mReadByte(CTRL_REG3_M);  
+          if (m & 0x01) {
+            tempRegValue &= 0xFC;   //set bits 1:0 to 0, keep other bits
+            imu.mWriteByte(CTRL_REG3_M, tempRegValue);
+          } else {
+            tempRegValue |= 0x03;   //set bits 1:0 to 1, keep other bits
+            imu.mWriteByte(CTRL_REG3_M, tempRegValue);
+          }
+        }
+        state_imu = m;
+      }
+    }
+  }
+  // Returns energyConsumed for use by outside world
+  float getEnergyConsumed() {
+      return energyConsumed;
+  }
+// Returns percentEnergyLeft for use by outside world
+  float getBatteryLeft() {
+      return percentEnergyLeft;
+  }
+  float getTotalElapsedTime() {
+    return millis() + time_offset;
+  }
+};
 
 class Selector
 {
@@ -259,6 +494,7 @@ class Angle
 
 Angle angle;
 Selector select;
+PowerMonitor pm(0x0F);
 
 
 void setup() {
@@ -267,6 +503,11 @@ void setup() {
   tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
   tft.setRotation(3);
   tft.fillScreen(ST7735_BLACK);
+
+  pinMode(WIFI_CH_PD_PIN, OUTPUT);
+  digitalWrite(WIFI_CH_PD_PIN, HIGH);
+  config.setTimer(TEENSY_SLEEP_TIME);   
+
 
   //button setup
 
@@ -360,20 +601,39 @@ void setup() {
 }
 
 void loop() {
+
+      bool select_button = !digitalRead(BUTTON_GREEN);
+      bool back_button = !digitalRead(BUTTON_WHITE);
+
+
   angle.update();
   if(movement){
       select.update(angle.pitch(),db_length);
     }
 
+  if(millis() - last_button_press > DORMANTINTERVAL){
+    //do sleep things here
+      pm.setPowerMode("wifi",0);
+      pm.setPowerMode("teensy",1);
+      tft.fillScreen(ST7735_BLACK);
+      movement = false;
+
+
+      if(select_button || back_button){
+          pm.setPowerMode("wifi",1);
+          pm.setPowerMode("teensy",2);
+          tft.fillScreen(ST7735_BLACK);
+          movement = true;
+          delay(15000);
+        }
+    }  
+
 
   //do some green button magic
 
-    bool select_button = !digitalRead(BUTTON_GREEN);
-
-    Serial.println("Button:");
-    Serial.println(select_button);
     
     if(select_button){
+      last_button_press = millis();
       previous_song_title = song_title; //Careful - there could be some problems with the next 2 lines...
       song_title=current_song;
       if(previous_song_title==song_title) {
@@ -399,11 +659,9 @@ void loop() {
     }
 
   //do some white button magic
-
-    bool back_button = !digitalRead(BUTTON_WHITE);
-    
       if(back_button){
       movement = true;
+      last_button_press = millis();
       }
       
         
